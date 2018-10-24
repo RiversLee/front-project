60c9222f65f72aa61b91b54c684ab062fa679212
River Lee <571970620@qq.com>
2018-10-24 19:33:46 +0800

    git push

--- nbaproject/scripts/main.js
+++ nbaproject/scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var NBA_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail'
+var ESC_KEY = 27;
 
 function setDetails(imageUrl,titleText) {
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -8,6 +10,7 @@ function setDetails(imageUrl,titleText) {
 
     detailImage.setAttribute('src',imageUrl);
     detailTitle.textContent = titleText;
+    
 }
 function imageFormThumb(nba) {
     return nba.getAttribute('data-image-url');
@@ -22,5 +25,32 @@ function addNbaClickHander(nba) {
     nba.addEventListener('click',function (event) {
        event.preventDefault();
        setDetailsFromNba(nba); 
+       showDetail();
     });
 }
+function getNbaArray() {
+    var nba = document.querySelectorAll(NBA_LINK_SELECTOR)
+    var nbaArray = [].slice.call(nba);
+    return nbaArray;
+}
+function hideDetail() {
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetail(){
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+function addKeyPressHandler() {
+    document.body.addEventListener('keyup',function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if(event.keyCode == 27){
+            hideDetail();
+        }
+    })
+}
+function initializeEvents() {
+    var array = getNbaArray();
+    array.forEach(addNbaClickHander);
+    addKeyPressHandler();
+}
+initializeEvents();
