f534a37168d80434c3c97111f7d03c392919dce4
River Lee <571970620@qq.com>
2018-11-01 15:18:36 +0800

    实现了数据的抓取

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,7 +1,13 @@
 (function (window) {
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
     var App = window.App;
+    var FormHandler = App.FormHandler;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var MyTruck = new Truck('ncc-1701',new DataStore());
     window.MyTruck = MyTruck;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSumitHandler(MyTruck.createOrder.bind(MyTruck));
+    console.log(formHandler);
 })(window);
